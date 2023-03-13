01c515e602219b8c2d8e02d2cb1e5550f5b7b18f
Divante Parness <divante111@gmail.com>
2023-03-12 21:19:50 -0500

    completed more coding bat practice questions python

--- codingbat_practice_udemy.py
+++ codingbat_practice_udemy.py
@@ -52,3 +52,72 @@ def diff21(n):
     return 21 - n
   elif n == 21:
     return 0
+  
+# We have a loud talking parrot. The "hour" parameter 
+# is the current hour time in the range 0..23.
+# We are in trouble if the parrot is talking and the hour 
+# is before 7 or after 20. Return True if we are in trouble.
+
+def parrot_trouble(talking, hour):
+  if talking == True and hour < 7 or talking == True and hour > 20:
+    return True
+  elif talking == True and hour > 7 or talking == True and hour < 20:
+    return False
+  elif talking == False and hour > 20 or talking == False and hour < 7:
+    return False
+  elif talking == False and hour > 7 or talking == False and hour < 20:
+    return False
+  
+#   Given 2 ints, a and b, return True if one if them is 10 or 
+#   if their sum is 10.
+
+def makes10(a, b):
+  if a == 10 or b == 10:
+    return True
+  elif a + b == 10:
+    return True
+  else:
+    return False
+  
+# Given an int n, return True if it is within 10 of 100 or 200. 
+# Note: abs(num) computes the absolute value of a number.
+
+def near_hundred(n):
+  if n < 111 and n > 89:
+    return True
+  elif n < 211 and n > 189:
+    return True
+  else:
+    return False
+  
+# Given 2 int values, return True if one is negative and 
+# one is positive. Except if the parameter "negative" is True, 
+# then return True only if both are negative.
+
+def pos_neg(a, b, negative):
+  if a < 0 and b < 0 and negative == True:
+    return True
+  elif a > 0 and b < 0 and negative == False:
+    return True
+  elif a < 0 and b > 0 and negative == False:
+    return True
+  elif a > 0 and b > 0 and negative == False:
+    return False
+  elif a < 0 and b < 0 and negative == False:
+    return False
+  elif a < 0 and b > 0 and negative == True:
+    return False
+  elif a > 0 and b > 0 and negative == True:
+    return False
+  elif a > 0 and b < 0 and negative == True:
+    return False
+  
+# Given a string, return a new string where "not " has been added 
+# to the front. However, if the string already begins with "not", 
+# return the string unchanged.
+
+def not_string(str):
+  if str[0:3] != 'not':
+    return 'not ' + str
+  elif str[0:3] == 'not':
+    return str
