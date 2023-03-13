13758e127e7e986a48dbfc257a83670f5858ceaf
Divante Parness <divante111@gmail.com>
2023-03-13 09:54:48 -0500

    did some python problems codingbat

--- codingbat_practice_udemy.py
+++ codingbat_practice_udemy.py
@@ -239,3 +239,28 @@ def hello_name(name):
 
 def make_abba(a, b):
   return a +b + b + a
+
+
+# The web is built with HTML strings like "<i>Yay</i>" which 
+# draws Yay as italic text. In this example, the "i" tag makes
+# <i> and </i> which surround the word "Yay". Given tag and word 
+# strings, create the HTML string with tags around the word, e.g. 
+# "<i>Yay</i>".
+
+def make_tags(tag, word):
+  return '<'+ tag + '>' + word + '</' + tag + '>'
+
+
+# Given an "out" string length 4, such as "<<>>", and a word, 
+# return a new string where the word is in the middle of the 
+# out string, e.g. "<<word>>".
+
+def make_out_word(out, word):
+  return out[:2] + word + out[2:]
+
+# Given a string, return a new string made of 3 copies of the 
+# last 2 chars of the original string. The string length will 
+# be at least 2.
+
+def extra_end(str):
+  return str[len(str)-2: len(str)] * 3
